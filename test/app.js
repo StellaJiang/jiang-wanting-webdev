97ba9857b3cd6b58cb35a682ee4545a052a398a9
stella <stella@ccd.neu.edu>
2016-09-17 14:51:50 -0700

    connect to mongolab

--- test/app.js
+++ test/app.js
@@ -4,8 +4,8 @@ module.exports = function(app)
     app.post("/api/test", createMessage);
     app.delete("/api/test/:id", deleteMessage);
 
-    var uri = "mongodb://stella:Jwt!19880906@cluster0-shard-00-00-7yexl.mongodb.net:27017,cluster0-shard-00-01-7yexl.mongodb.net:27017,cluster0-shard-00-02-7yexl.mongodb.net:27017/admin?ssl=true&replicaSet=Cluster0-shard-0&database=test&authSource=admin";
-
+    //var uri = "mongodb://stella:Jwt!19880906@cluster0-shard-00-00-7yexl.mongodb.net:27017,cluster0-shard-00-01-7yexl.mongodb.net:27017,cluster0-shard-00-02-7yexl.mongodb.net:27017/admin?ssl=true&replicaSet=Cluster0-shard-0&database=test&authSource=admin";
+    var uri = "mongodb://stella:Jwt!19880906@ds033116.mlab.com:33116/stellajiang";
     var mongoose = require("mongoose");
     mongoose.Promise = global.Promise;
     mongoose.connect(uri);
