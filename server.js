44638d1536263a6d722d008d2c1b9097d5fc731f
stella <stella@ccd.neu.edu>
2016-12-10 15:36:16 -0800

    change http to https in omdb url

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ app.use(express.static(__dirname + '/public'));
 //require ("./test/app.js")(app);
 //require("./assignment/app.js")(app);
 require("./project/app.js")(app);
-require("./sandbox/http/proxy")(app);
 
 var ipaddress = process.env.IP;
 var port      = process.env.PORT || 3000;
